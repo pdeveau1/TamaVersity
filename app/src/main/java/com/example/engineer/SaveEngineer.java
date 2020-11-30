07291b782e82900bc6a3e65ebb060f00b1d7d69c
pdeveau1 <pdeveau@bu.edu>
2020-11-30 17:12:30 -0500

    starting save code

---
+++ app/src/main/java/com/example/engineer/SaveEngineer.java
@@ -0,0 +1,16 @@
+package com.example.engineer;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SaveEngineer extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
