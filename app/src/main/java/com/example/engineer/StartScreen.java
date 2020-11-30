36ea7fe1eedf3f67b181218ae05c2cfc65f99164
leslyalc <leslyalc12@gmail.com>
2020-11-30 18:12:44 -0500

    Start Screen :)

--- app/src/main/java/com/example/engineer/StartScreen.java
+++ app/src/main/java/com/example/engineer/StartScreen.java
@@ -2,6 +2,7 @@ package com.example.engineer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class StartScreen extends AppCompatActivity {
@@ -10,5 +11,33 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        Thread thread = new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    sleep(5000);
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    Intent mainIntent = new Intent(StartScreen.this, MainActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        finish();
     }
 }
