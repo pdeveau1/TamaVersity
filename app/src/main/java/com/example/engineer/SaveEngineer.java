7623294c659478a3200d4e377886e71a8a3937f9
pdeveau1 <pdeveau@bu.edu>
2020-11-30 18:03:19 -0500

    save code

--- app/src/main/java/com/example/engineer/SaveEngineer.java
+++ app/src/main/java/com/example/engineer/SaveEngineer.java
@@ -1,16 +1,40 @@
 package com.example.engineer;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SaveEngineer extends AppCompatActivity
 {
+    TextView tv_academic;
+
+    float lastAcademic, lastHealth, lastSocial;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saveengineer);
+
+        tv_academic = (TextView) findViewById(R.id.tv_academic);
+
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        lastAcademic = preferences.getFloat("lastAcademic",0);
+        lastHealth = preferences.getFloat("lastHealth",0);
+        lastSocial = preferences.getFloat("lastSocial",0);
+
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
