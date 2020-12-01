76c26b8a8d4f322057a3ee5e11a541241fe68b9d
pdeveau1 <pdeveau@bu.edu>
2020-11-30 21:09:37 -0500

    add x to bring to end screen

---
+++ app/src/main/java/com/example/engineer/EndActivity.java
@@ -0,0 +1,14 @@
+package com.example.engineer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end);
+    }
+}
