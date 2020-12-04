9e74e6d8b79946d7e7a2f5dd0db50b0784f80c77
pdeveau1 <pdeveau@bu.edu>
2020-12-04 14:04:24 -0500

    StartActivity first thing to open

--- app/src/main/java/com/example/engineer/MainActivity.java
+++ app/src/main/java/com/example/engineer/MainActivity.java
@@ -1,22 +1,14 @@
 package com.example.engineer;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-
 import java.util.Timer;
 import java.util.TimerTask;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
 public class MainActivity extends AppCompatActivity
 {
     private TakeCareView gameView;
@@ -32,7 +24,6 @@ public class MainActivity extends AppCompatActivity
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         TakeCare engineer = (TakeCare)getIntent().getSerializableExtra("Engineer");
-
 
         //gameView = new TakeCareView(this, engineer);
         gameView = new TakeCareView(this, engineer);
