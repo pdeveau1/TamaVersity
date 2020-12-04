eb0ee7ac7dd70faa23d43bc4436f8a623615afb2
pdeveau1 <pdeveau@bu.edu>
2020-12-04 14:48:35 -0500

    working on getting date

--- app/src/main/java/com/example/engineer/DateActivity.java
+++ app/src/main/java/com/example/engineer/DateActivity.java
@@ -1,6 +1,5 @@
 package com.example.engineer;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -30,7 +29,8 @@ public class DateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_date);
         TakeCare engineer = (TakeCare)getIntent().getSerializableExtra("Engineer");
 
-        tvw=(TextView)findViewById(R.id.textView1);
+        tvw=(TextView)findViewById(R.id.tvDate);
+        tvw1=(TextView)findViewById(R.id.tvDate1);
         picker=(DatePicker)findViewById(R.id.datePicker1);
         btnGet=(Button)findViewById(R.id.button1);
         btnGet.setOnClickListener(new View.OnClickListener()
@@ -39,28 +39,30 @@ public class DateActivity extends AppCompatActivity {
             public void onClick(View v)
             {
                 tvw.setText("Selected Date: "+ picker.getDayOfMonth()+"-"+ (picker.getMonth() + 1)+"-"+picker.getYear());
+                String dateString = $.datepicker.formatDate("dd-mm-yy", picker);
                 String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
                 tvw.setText(currentDate);
 
                 //saves date semester ends
                 //date format:dd-MM-yyyy
-                String date = picker.getDayOfMonth()+""+(picker.getMonth() + 1)+""+picker.getYear();
+                String date = picker.getDayOfMonth()+"-"+(picker.getMonth() + 1)+"-"+picker.getYear();
+                tvw1.setText(date);
                 SharedPreferences preferences = getSharedPreferences("PREFS",0);
                 SharedPreferences.Editor editor = preferences.edit();
                 editor.putString("Date",date);
                 editor.apply();
 
-                Intent intent = new Intent(getApplicationContext(), SaveEngineer.class);
-                startActivity(intent);
-                finish();
+//                Intent intent = new Intent(getApplicationContext(), SaveEngineer.class);
+//                startActivity(intent);
+//                finish();
 
-                //takes to main game
-                Intent mainIntent = new Intent(DateActivity.this, MainActivity.class);
-                //passes to mainActivity the engineer
-                mainIntent.putExtra("Engineer", engineer);
-                mainIntent.putExtra("Date", date);
-                //starts main activity
-                startActivity(mainIntent);
+//                //takes to main game
+//                Intent mainIntent = new Intent(DateActivity.this, MainActivity.class);
+//                //passes to mainActivity the engineer
+//                mainIntent.putExtra("Engineer", engineer);
+//                mainIntent.putExtra("Date", date);
+//                //starts main activity
+//                startActivity(mainIntent);
             }
         });
     }
