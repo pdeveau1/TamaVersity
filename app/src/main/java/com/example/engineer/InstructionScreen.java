361eb7448dffcc36377f5087fc98a4640c3909e8
pdeveau1 <pdeveau@bu.edu>
2020-12-05 21:52:04 -0500

    working on instruction screen

---
+++ app/src/main/java/com/example/engineer/InstructionScreen.java
@@ -0,0 +1,14 @@
+package com.example.engineer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InstructionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction_screen);
+    }
+}
