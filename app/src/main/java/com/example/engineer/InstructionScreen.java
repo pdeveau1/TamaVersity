9f79280d4e7dc5dfd48cf95cd45548e090296978
pdeveau1 <pdeveau@bu.edu>
2020-12-05 23:05:52 -0500

    click out of instruction screen

--- app/src/main/java/com/example/engineer/InstructionScreen.java
+++ app/src/main/java/com/example/engineer/InstructionScreen.java
@@ -1,14 +1,32 @@
 package com.example.engineer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InstructionScreen extends AppCompatActivity {
+public class InstructionScreen extends AppCompatActivity{
+    GestureDetector gestureDetector;
+
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction_screen);
+        //gestureDetector = new GestureDetector(InstructionScreen.this, InstructionScreen.this);
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+//        this.gestureDetector.onTouchEvent(event);
+//        // Be sure to call the superclass implementation
+//        return super.onTouchEvent(event);
+        Intent loadIntent = new Intent(InstructionScreen.this, LoadActivity.class);
+        startActivity(loadIntent);
+        return false;
     }
 }
--- app/src/main/java/com/example/engineer/StartActivity.java
+++ app/src/main/java/com/example/engineer/StartActivity.java
@@ -48,7 +48,7 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void run() {
                 //creates new intent to go from StartActivity to LoadActivity
-                Intent intent = new Intent(StartActivity.this, LoadActivity.class);
+                Intent intent = new Intent(StartActivity.this, InstructionScreen.class);
                 startActivity(intent);
                 finish();
             }
