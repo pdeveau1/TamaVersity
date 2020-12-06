222c91afe112842e23cf89815e53b4261c42aba1
pdeveau1 <pdeveau@bu.edu>
2020-12-05 20:01:57 -0500

    fix load screen

--- app/src/main/java/com/example/engineer/FinishActivity.java
+++ app/src/main/java/com/example/engineer/FinishActivity.java
@@ -8,10 +8,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class FinishActivity extends AppCompatActivity {
     private TakeCare engineer;
-    private FinalReport finalR;
     TextView socialGrade;
     TextView academicGrade;
     TextView healthGrade;
+
+    String calcGrade(float grade)
+    {
+        int score = (int)grade;
+        switch(score/10)
+        {
+            case 10:
+                return "A+";
+            case 9:
+                return "A";
+            case 8:
+                return "B";
+            case 7:
+                return "C";
+            case 6:
+                return "D";
+            default:
+                return "F";
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,8 +44,8 @@ public class FinishActivity extends AppCompatActivity {
         academicGrade = (TextView) findViewById(R.id.academicGrade);
         healthGrade = (TextView) findViewById(R.id.healthGrade);
 
-        socialGrade.setText(finalR.calcGrade(engineer.getSocial()));
-        academicGrade.setText(finalR.calcGrade(engineer.getAcademic()));
-        healthGrade.setText(finalR.calcGrade(engineer.getHealth()));
+        socialGrade.setText(this.calcGrade(engineer.getSocial()));
+        academicGrade.setText(this.calcGrade(engineer.getAcademic()));
+        healthGrade.setText(this.calcGrade(engineer.getHealth()));
     }
 }
